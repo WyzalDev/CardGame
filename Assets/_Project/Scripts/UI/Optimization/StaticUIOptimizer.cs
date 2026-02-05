013ac8a1ab678d56ab3bb9b6f731c851597b382d
Kiryl Bulakh <wyzaldev@gmail.com>
2026-02-05 13:47:02 +0300

    Layout Group optimization for static ui added

---
+++ Assets/_Project/Scripts/UI/Optimization/StaticUIOptimizer.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.UI.Optimization
+{
+    public class StaticUIOptimizer : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _staticUIRoot;
+
+        private LayoutGroup[] _layoutGroups;
+
+        private void Awake()
+        {
+            _layoutGroups = GetComponentsInChildren<LayoutGroup>();
+        }
+
+        private void Start()
+        {
+            RebuildStaticUI();
+        }
+
+        public void RebuildStaticUI()
+        {
+            RebuildAsync().Forget();
+        }
+
+        private async UniTaskVoid RebuildAsync()
+        {
+            foreach (var layoutGroup in _layoutGroups)
+                layoutGroup.enabled = true;
+
+            await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate);
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(_staticUIRoot);
+
+            foreach (var layoutGroup in _layoutGroups)
+                layoutGroup.enabled = false;
+        }
+    }
+}
